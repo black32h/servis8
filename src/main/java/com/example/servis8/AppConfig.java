529cbabd77f9bf441af29dbd8831f0f048e5a1ea
black32h <blackspetya@gmail.com>
2025-01-12 11:55:34 +0200

    Update AppConfig.java

--- src/main/java/com/example/servis8/AppConfig.java
+++ src/main/java/com/example/servis8/AppConfig.java
@@ -1,4 +1,16 @@
 package com.example.servis8;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class AppConfig {
+
+    // Створення біну RestTemplate для використання в інших компонентах
+    @Bean
+    public RestTemplate restTemplate() {
+        // Повертаємо новий екземпляр RestTemplate для використання в сервісах
+        return new RestTemplate();
+    }
 }
